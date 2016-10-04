289ecc6a7a2f89aa6778819f20a1e42610fe36b1
Lucas Rangel Cezimbra <lrcezimbra@hotmail.com>
2016-10-04 00:49:24 -0300

    Email ou telefone epcionais

--- eventex/subscriptions/tests/test_form_subscription.py
+++ eventex/subscriptions/tests/test_form_subscription.py
@@ -3,24 +3,38 @@ from eventex.subscriptions.forms import SubscriptionForm
 
 class SubscriptionFormTest(TestCase):
     def test_form_has_fields(self):
-        """Form must have 4 fields"""
         form = SubscriptionForm()
         expected = ['name', 'cpf', 'email', 'phone']
         self.assertSequenceEqual(expected, list(form.fields))
 
     def test_cpf_is_digit(self):
-        """CPF must only accept digits"""
         form = self.make_validated_form(cpf='ABCD5678901')
         self.assertFormErrorCode(form, 'cpf', 'digits')
 
     def test_cpf_has_11_digits(self):
-        """CPF must have 11 digits"""
         form = self.make_validated_form(cpf='1234')
         self.assertFormErrorCode(form, 'cpf', 'length')
 
     def test_name_must_be_capitalized(self):
         form = self.make_validated_form(name='LUCAS rangel cEZIMBRA')
         self.assertEqual('Lucas Rangel Cezimbra', form.cleaned_data['name'])
+
+    def test_email_is_optional(self):
+        form = self.make_validated_form(email='')
+        self.assertFalse(form.errors)
+
+    def test_phone_is_optional(self):
+        form = self.make_validated_form(phone='')
+        self.assertFalse(form.errors)
+
+    def test_must_inform_email_or_phone(self):
+        form = self.make_validated_form(email='', phone='')
+        self.assertListEqual(['__all__'], list(form.errors))
+        self.assertFormErrorMessage(form, '__all__', 'Informe seu e-mail ou telefone.')
+
+    def test_error_when_email_invalid_and_phone_empty(self):
+        form = self.make_validated_form(email='asdfsdf', phone='')
+        self.assertFormErrorMessage(form, '__all__', 'Informe seu e-mail ou telefone.')
 
     def assertFormErrorCode(self, form, field, code):
         errors = form.errors.as_data()
