f55d9ecfc9d0e719bb1181174b947a79e4cf88df
Lucas Rangel Cezimbra <lrcezimbra@hotmail.com>
2016-10-05 00:50:17 -0300

    Add speakers

--- eventex/core/tests/test_view_home.py
+++ eventex/core/tests/test_view_home.py
@@ -6,13 +6,27 @@ class HomeTest(TestCase):
         self.response = self.client.get(r('home'))
 
     def test_get(self):
-        """GET / must return status code 200"""
         self.assertEqual(200, self.response.status_code)
 
     def test_template(self):
-        """Must use index.html"""
         self.assertTemplateUsed(self.response, 'index.html')
 
     def test_subscription_link(self):
         expected = 'href="{}"'.format(r('subscriptions:new'))
+        self.assertContains(self.response, expected)
+
+    def test_speakers(self):
+        contents = [
+                'Grace Hopper',
+                'http://hbn.link/hopper-pic',
+                'Alan Turing',
+                'http://hbn.link/turing-pic'
+        ]
+
+        for expected in contents:
+            with self.subTest():
+                self.assertContains(self.response, expected)
+
+    def test_speakers_link(self):
+        expected = 'href="{}#speakers"'.format(r('home'))
         self.assertContains(self.response, expected)
